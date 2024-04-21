f358f53a7aaa5bf00e93b537d46b9c28d984d962
jinwook567 <jinwook567@we2d.com>
2024-04-22 02:11:20 +0900

    feat: interquartile-range

---
+++ src/ko_premium_algo/analyze.clj
@@ -0,0 +1,12 @@
+(ns ko-premium-algo.analyze)
+
+(defn interquartile-range [numbers]
+  (let [sorted-numbers (vec (sort numbers))
+        len (count sorted-numbers)
+        Q1 (nth sorted-numbers (Math/floor (/ len 4)))
+        Q3 (nth sorted-numbers (Math/floor (* 3 (/ len 4))))
+        IQR (- Q3 Q1)
+        LIF (- Q1 (* 1.5 IQR))
+        UIF (+ Q3 (* 1.5 IQR))]
+    (fn [arg]
+      (and (>= arg LIF) (<= arg UIF)))))
