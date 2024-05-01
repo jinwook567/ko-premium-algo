b77b18da88730af5e93102c4174a63467c8fd406
jinwook567 <jinwook567@we2d.com>
2024-05-01 18:31:58 +0900

    feat: distribute-number
    숫자를 분배하는 함수

--- src/ko_premium_algo/distribution.clj
+++ src/ko_premium_algo/distribution.clj
@@ -2,3 +2,8 @@
 
 (defn distribute [f n seq]
   (reduce concat (map f (partition-all n seq))))
+
+(defn distribute-number [n num]
+  (if (>= n num)
+    (list num)
+    (cons n (distribute-number n (- num n)))))
