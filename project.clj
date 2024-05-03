13b56384ecd1838d8cd0a3a2800b6f65461c49d7
jinwook567 <jinwook567@we2d.com>
2024-05-04 04:39:05 +0900

    chore: install environ

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
                  [ring/ring-defaults "0.3.2"]
                  [clj-http "3.12.3"]
                  [cheshire "5.12.0"]
-                 ]
-  :plugins [[lein-ring "0.12.5"] [lein-cljfmt "0.9.2"]]
+                 [environ "1.2.0"]]
+  :plugins [[lein-ring "0.12.5"] [lein-cljfmt "0.9.2"] [lein-environ "1.2.0"]]
   :ring {:handler ko-premium-algo.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
