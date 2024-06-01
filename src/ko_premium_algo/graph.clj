a43584112d521946be60baa0d2205580fcb3aa56
jinwook567 <jinwook567@we2d.com>
2024-06-02 01:59:47 +0900

    refactor: optimal-route
    choice라는 함수를 받음으로써 더 유연하게 사용할 수 있도록 변경하였음.

--- src/ko_premium_algo/graph.clj
+++ src/ko_premium_algo/graph.clj
@@ -3,26 +3,21 @@
 (defn make-edge [weight start end]
   {:weight weight :start start :end end})
 
-(defn nodes [edges]
-  (seq
-   (reduce (fn [acc edge] (conj acc (:start edge) (:end edge))) #{} edges)))
+(defn weight [edge] (:weight edge))
 
-(defn optimal-exchange-info [edges]
-  (let [search (fn [short start end visited]
-                 (if (= start end) {:weight 1 :path [start]}
-                     (let [connected-edges (filter #(and (= (:end %) end) (not (contains? visited (:start %)))) edges)]
-                       (if (empty? connected-edges)
-                         {:weight Double/POSITIVE_INFINITY :path []}
-                         (let [connect-edge-from-start (fn [edge]
-                                                         (let [short-from-start (short start (:start edge) (conj visited (:start edge)))]
-                                                           {:weight (* (:weight short-from-start) (:weight edge)) :path (conj (:path short-from-start) (:end edge))}))]
-                           (reduce #(if (< (:weight %1) (:weight %2)) %1 %2) (map connect-edge-from-start connected-edges)))))))
+(defn start [edge] (:start edge))
+
+(defn end [edge] (:end edge))
+
+(defn optimal-route [edges, choice]
+  (let [search (fn [short start-node end-node visited]
+                 (let [destination-edges (filter #(and (= end-node (end %)) (not (contains? visited (start %)))) edges)
+                       shortest-route-to-node (fn [node] (short start-node node (conj visited node)))
+                       shortest-route-to-edge (fn [edge]
+                                                 (if (= start-node (start edge)) 
+                                                   [edge]
+                                                   (some-> (shortest-route-to-node (start edge)) (conj edge))))]
+                   (choice (map shortest-route-to-edge destination-edges))))
         memorized-search (memoize search)
         short (fn short [start end visited] (memorized-search short start end visited))]
     (memoize (fn [start end] (search short start end #{end})))))
-
-(defn exchange-rate [optimal-exchange-info]
-  (:weight optimal-exchange-info))
-
-(defn path [optimal-exchange-info]
-  (:path optimal-exchange-info))
