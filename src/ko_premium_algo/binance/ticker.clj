62c40ee75b0f1b4730cbdd2f584a57d3a5d617bb
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2024-11-17 21:58:59 +0900

    feat: binance ticker

---
+++ src/ko_premium_algo/binance/ticker.clj
@@ -0,0 +1,15 @@
+(ns ko-premium-algo.binance.ticker
+  (:require [ko-premium-algo.trade.ticker :refer [make-ticker]]
+            [ko-premium-algo.trade.market :refer [symbol]]
+            [cheshire.core :as json]
+            [clojure.string :as str]
+            [clj-http.client :as client]))
+
+(defn- ticker-symbols [symbols]
+  (str "[" (str/join "," (map #(str "\"" % "\"") symbols)) "]"))
+
+(defn ticker [markets]
+  (->> (map symbol markets)
+       (#(client/get "https://api.binance.com/api/v3/ticker/price" {:query-params {"symbols" (ticker-symbols %)}}))
+       (#(json/parse-string (:body %)))
+       (map #(make-ticker %1 (Double/parseDouble (get %2 "price"))) markets)))
