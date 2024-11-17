5678f13d587264741d1dc7e17180d4aad987dd80
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2024-11-17 21:16:36 +0900

    feat: binance markets

---
+++ src/ko_premium_algo/binance/markets.clj
@@ -0,0 +1,10 @@
+(ns ko-premium-algo.binance.markets
+  (:require [clj-http.client :as client]
+            [cheshire.core :as json]
+            [ko-premium-algo.trade.market :refer [make-market]]))
+
+(defn markets []
+  (->> (:body (client/get "https://api.binance.com/api/v3/exchangeInfo?permissions=SPOT"))
+       json/parse-string
+       (#(get % "symbols"))
+       (map #(make-market (get % "baseAsset") (get % "quoteAsset") (get % "symbol")))))
