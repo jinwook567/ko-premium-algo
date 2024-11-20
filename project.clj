9e0e35f30871e5dd5bad89e05da7c2542e3caf30
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2024-11-21 01:37:16 +0900

    install buddy

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
                  [ring/ring-defaults "0.3.2"]
                  [clj-http "3.12.3"]
                  [cheshire "5.12.0"]
-                 [environ "1.2.0"]]
+                 [environ "1.2.0"]
+                 [buddy/buddy-sign "3.1.0"]
+                 [buddy/buddy-core "1.12.0-430"]]
   :plugins [[lein-ring "0.12.5"] [lein-cljfmt "0.9.2"] [lein-environ "1.2.0"]]
   :ring {:handler ko-premium-algo.handler/app}
   :profiles
