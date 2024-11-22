d9d82a4c34b0e7ff8ccf3d2bdf72bc48e18a52ee
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2024-11-23 00:49:10 +0900

    feat: trade terms

---
+++ src/ko_premium_algo/trade/terms.clj
@@ -0,0 +1,19 @@
+(ns ko-premium-algo.trade.terms)
+
+(defn make-terms [fee limits]
+  {:fee fee :limits limits})
+
+(defn fee [terms]
+  (:fee terms))
+
+(defn limits [terms]
+  (:limits terms))
+
+(defn make-market-terms [ask-terms bid-terms]
+  {:ask-terms ask-terms :bid-terms bid-terms})
+
+(defn ask-terms [terms]
+  (:ask-terms terms))
+
+(defn bid-terms [terms]
+  (:bid-terms terms))
