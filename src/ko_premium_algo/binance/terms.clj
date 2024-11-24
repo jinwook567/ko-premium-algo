2b58ab8d67020737caca19eca806bd6c87ed3d7f
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2024-11-25 00:58:42 +0900

    feat: binance terms

---
+++ src/ko_premium_algo/binance/terms.clj
@@ -0,0 +1,42 @@
+(ns ko-premium-algo.binance.terms
+  (:require [clj-http.client :as client]
+            [ko-premium-algo.trade.market :as m]
+            [ko-premium-algo.trade.fee :refer [make-fee]]
+            [ko-premium-algo.binance.auth :as auth]
+            [ko-premium-algo.lib.range :refer [make-range]]
+            [ko-premium-algo.trade.terms :refer [make-market-terms make-terms]]
+            [ko-premium-algo.trade.limits :refer [make-limits]]
+            [cheshire.core :as json]))
+
+(defn- fee [market]
+  (->> (client/get "https://api.binance.com/sapi/v1/asset/tradeFee"
+                   {:headers (auth/add-secret-key {})
+                    :query-params (auth/make-payload {:symbol (m/symbol market)})})
+       (#(first (json/parse-string (:body %))))
+       (#(make-fee :rate (Float/parseFloat (get % "makerCommission"))))))
+
+
+(defn- make-qty-range [filters]
+  (let [qty-filter (some #(when (= (get % "filterType") "LOT_SIZE") %) filters)]
+    (make-range (Float/parseFloat (get qty-filter "minQty"))
+                (Float/parseFloat (get qty-filter "maxQty"))
+                (Float/parseFloat (get qty-filter "stepSize")))))
+
+(defn- make-amount-range [filters]
+  (let [amount-filter (some #(when (= (get % "filterType") "NOTIONAL") %) filters)]
+    (make-range (Float/parseFloat (get amount-filter "minNotional"))
+                (Float/parseFloat (get amount-filter "maxNotional"))
+                nil)))
+
+(defn- limits [market]
+  (->> (client/get "https://api.binance.com/api/v3/exchangeInfo?permissions=SPOT"
+                   {:query-string {"symbol" (m/symbol market)}})
+       (#(json/parse-string (:body %)))
+       (#(first (get % "symbols")))
+       (#(get % "filters"))
+       (#(make-limits (make-qty-range %) (make-amount-range %)))))
+
+(defn terms [market]
+  (make-market-terms
+   (make-terms (fee market) (limits market))
+   (make-terms (fee market) (limits market))))
