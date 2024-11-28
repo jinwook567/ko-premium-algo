bd87804517bc42bad8939e69cc43c605119672cc
이진욱 <dlwlsdnr567@naver.com>
2024-11-29 02:20:51 +0900

    feat: binance order

---
+++ src/ko_premium_algo/binance/order.clj
@@ -0,0 +1,61 @@
+(ns ko-premium-algo.binance.order
+  (:require [ko-premium-algo.trade.order :refer [make-order]]
+            [ko-premium-algo.trade.intent :refer [make-intent market side qty price]]
+            [ko-premium-algo.lib.time :refer [millis->time]]
+            [clj-http.client :as client]
+            [ko-premium-algo.trade.market :as m]
+            [ko-premium-algo.binance.auth :as auth]
+            [cheshire.core :as json]))
+
+(defn- order-side [binance-side]
+  (if (= binance-side "BUY") :bid :ask))
+
+(defn- binance-side [intent-side]
+  (if (= intent-side :bid) "BUY" "SELL"))
+
+(defn- order-state [binance-state]
+  (cond
+    (= binance-state "NEW") :open
+    (= binance-state "PARTIALLY_FILLED") :open
+    (= binance-state "FILLED") :done
+    (or (= binance-state "CANCELED")
+        (= binance-state "REJECTED")
+        (= binance-state "EXPIRED")
+        (= binance-state "EXPIRED_IN_MATCH")) :cancelled
+    :else (throw (ex-info "invalid state" {:state binance-state}))))
+
+(defn- response->order [market response]
+  (make-order (get response "orderId")
+              (make-intent market
+                           (order-side (get response "side"))
+                           (Float/parseFloat (get response "origQty"))
+                           (Float/parseFloat (get response "price")))
+              (Float/parseFloat (get response "executedQty"))
+              (millis->time (get response "time"))
+              (order-state (get response "status"))))
+
+(defn execute-order [intent]
+  (let [request {:symbol (m/symbol (market intent))
+                 :side (binance-side (side intent))
+                 :type "LIMIT"
+                 :quantity (qty intent)
+                 :price (price intent)
+                 :newOrderRespType "RESULT"}]
+    (->> (client/post "https://api.binance.com/api/v3/order"
+                      {:body (auth/make-payload request)
+                       :headers (auth/make-auth-header)}))
+    (#(json/parse-string (:body %)))
+    (#(response->order (market intent) %))))
+
+(defn open-orders [market]
+  (->> (client/get "https://api.binance.com/api/v3/openOrders"
+                   {:query-params (auth/make-payload {:symbol (m/symbol market)})
+                    :headers (auth/make-auth-header)})
+       (#(json/parse-string (:body %)))
+       (map #(response->order market %))))
+
+(defn order [market id]
+  (->> (client/get "https://api.binance.com/api/v3/order"
+                   {:query-params (auth/make-payload {:symbol (m/symbol market) :orderId id})}
+                   (#(json/parse-string (:body %)))
+                   (#(response->order market %)))))
