7429dbafe44c058b27d717f0e110c21cc459f6bc
이진욱 <dlwlsdnr567@naver.com>
2024-11-29 02:26:41 +0900

    etc: 불필요한 파일 제거

--- src/ko_premium_algo/distribution.clj
+++
@@ -1,9 +0,0 @@
-(ns ko-premium-algo.distribution)
-
-(defn distribute [f n seq]
-  (reduce concat (map f (partition-all n seq))))
-
-(defn distribute-number [n num]
-  (if (>= n num)
-    (list num)
-    (cons n (distribute-number n (- num n)))))
