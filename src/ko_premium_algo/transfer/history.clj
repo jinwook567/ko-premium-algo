a04f329cfdcfc8ace1a1825b23f997c7640b0c34
이진욱 <dlwlsdnr567@naver.com>
2024-12-06 02:10:53 +0900

    feat: transfer history txid 추가

--- src/ko_premium_algo/transfer/history.clj
+++ src/ko_premium_algo/transfer/history.clj
@@ -1,10 +1,13 @@
 (ns ko-premium-algo.transfer.history)
 
-(defn make-history [id intent created-at state]
-  {:id id :intent intent :created-at created-at :state state})
+(defn make-history [id txid intent created-at state]
+  {:id id :txid txid :intent intent :created-at created-at :state state})
 
 (defn id [history]
   (:id history))
+
+(defn txid [history]
+  (:txid history))
 
 (defn intent [history]
   (:intent history))
