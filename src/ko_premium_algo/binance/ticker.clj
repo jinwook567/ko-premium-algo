d2163bbb15623beeaf1047f0ff49db3e483149a9
이진욱 <dlwlsdnr567@naver.com>
2025-01-04 03:59:20 +0900

    feat: binance ticker api 분배 호출

--- src/ko_premium_algo/binance/ticker.clj
+++ src/ko_premium_algo/binance/ticker.clj
@@ -8,8 +8,11 @@
 (defn- ticker-symbols [symbols]
   (str "[" (str/join "," (map #(str "\"" % "\"") symbols)) "]"))
 
-(defn ticker [markets]
+(defn base-ticker [markets]
   (->> (map symbol markets)
        (#(client/get "https://api.binance.com/api/v3/ticker/price" {:query-params {"symbols" (ticker-symbols %)}}))
        (#(json/parse-string (:body %)))
        (map #(make-ticker %1 (Double/parseDouble (get %2 "price"))) markets)))
+
+(defn ticker [markets]
+  (flatten (pmap base-ticker (partition 500 markets))))
