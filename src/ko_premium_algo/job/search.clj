2cbb3ccab582b9c226e5f9cc8dc4e2b374ee369b
이진욱 <dlwlsdnr567@naver.com>
2025-01-18 00:25:05 +0900

    feat: search bridge-edges 적용

--- src/ko_premium_algo/job/search.clj
+++ src/ko_premium_algo/job/search.clj
@@ -2,16 +2,14 @@
   (:require [ko-premium-algo.trade.market :refer [assets base-asset]]
             [ko-premium-algo.job.edge :refer [make-ask-edge make-bid-edge make-withdraw-edge make-node]]
             [ko-premium-algo.job.weight :refer [route-weight]]
-            [ko-premium-algo.route.edge :refer [nodes]]
             [ko-premium-algo.route.search :refer [make-highest-weight-route-finder]]
             [ko-premium-algo.wallet.terms :refer [fee limits]]
+            [ko-premium-algo.wallet.unit :as unit]
             [ko-premium-algo.wallet.limits :refer [actions can-transfer?]]
             [ko-premium-algo.trade.fee :refer [value]]
-            [ko-premium-algo.lib.seq :refer [cartesian-product]]
             [ko-premium-algo.gateway.markets :refer [markets]]
             [ko-premium-algo.gateway.ticker :refer [ticker]]
             [ko-premium-algo.gateway.transfer :as transfer]
-            [clojure.set :refer [union]]
             [ko-premium-algo.trade.ticker :refer [market]]))
 
 (def assets-to-exclude #{"TON"})
@@ -26,28 +24,17 @@
   (concat (map #(make-ask-edge exchange %) tickers)
           (map #(make-bid-edge exchange %) tickers)))
 
-(defn add-method [method terms]
-  (merge {:method method} terms))
+(defn can-transfer-terms? [base-terms quote-terms]
+  (and (some? base-terms)
+       (some? quote-terms)
+       (can-transfer? (actions (limits base-terms)) (actions (limits quote-terms)))))
 
-(defn can-transfer-terms-list [base-method-terms-list quote-method-terms-list]
-  (->> (cartesian-product base-method-terms-list quote-method-terms-list)
-       (filter #(= (:method (first %)) (:method (second %))))
-       (filter #(can-transfer? (actions (limits (first %))) (actions (limits (second %)))))
-       (map first)))
-
-(defn make-bridge-edge [base-exchange quote-exchange transferable-method-terms-list asset]
-  (->> transferable-method-terms-list
-       (map (fn [terms] {:fee (value (fee terms)) :method (:method terms)}))
-       (reduce #(min-key :fee %1 %2) {:fee Double/POSITIVE_INFINITY :method nil})
-       (#(make-withdraw-edge base-exchange quote-exchange (:fee %) asset (:method %)))))
-
-(defn make-bridge-edges [base-exchange quote-exchange base-method-terms-list quote-method-terms-list asset]
-  (list (make-bridge-edge base-exchange quote-exchange (can-transfer-terms-list base-method-terms-list quote-method-terms-list) asset)
-        (make-bridge-edge quote-exchange base-exchange (can-transfer-terms-list quote-method-terms-list base-method-terms-list) asset)))
-
-(defn make-fake-bridge-edges [base-exchange quote-exchange asset]
-  (list (make-withdraw-edge base-exchange quote-exchange 0 asset :fake)
-        (make-withdraw-edge quote-exchange base-exchange  0 asset :fake)))
+(defn make-bridge-edges [base-exchange quote-exchange units base-terms-list quote-terms-list]
+  (->> (map vector base-terms-list quote-terms-list units)
+       (filter (fn [[base-terms quote-terms _]]
+                 (can-transfer-terms? base-terms quote-terms)))
+       (map (fn [[base-terms _ unit]]
+              (make-withdraw-edge base-exchange quote-exchange (value (fee base-terms)) (unit/asset unit) (unit/method unit))))))
 
 (defn make-one-way-graph [base-node quote-node]
   (let [tickers (fn [exchange] (ticker exchange (remove-markets-with-assets assets-to-exclude (markets exchange))))
@@ -55,15 +42,19 @@
         quote-tickers (filter #(= (base-asset (market %)) (:asset quote-node)) (tickers (:exchange quote-node)))
         base-edges (make-exchange-edges (:exchange base-node) base-tickers)
         quote-edges (make-exchange-edges (:exchange quote-node) quote-tickers)
-        method-terms-list (fn [node] (map #(add-method % (transfer/terms (:exchange node) (:asset node) %))
-                                          (transfer/methods (:exchange node) (:asset node))))
-        bridge-edges (->> [base-edges quote-edges]
-                          (map #(apply union (map nodes %)))
-                          (apply cartesian-product)
-                          (filter #(= (:asset (first %)) (:asset (second %))))
-                          (map #(make-fake-bridge-edges (:exchange base-node) (:exchange quote-node) (:asset (first %))))
-                          flatten)]
-    (concat base-edges quote-edges bridge-edges)))
+        base-units (transfer/units (:exchange base-node))
+        quote-units (transfer/units (:exchange quote-node))
+        base-bridges (make-bridge-edges (:exchange base-node)
+                                        (:exchange quote-node)
+                                        base-units
+                                        (transfer/terms (:exchange base-node) base-units)
+                                        (transfer/terms (:exchange quote-node) base-units))
+        quote-bridges (make-bridge-edges (:exchange quote-node)
+                                         (:exchange base-node)
+                                         quote-units
+                                         (transfer/terms (:exchange quote-node) quote-units)
+                                         (transfer/terms (:exchange base-node) quote-units))]
+    (concat base-edges quote-edges base-bridges quote-bridges)))
 
 (defn find-route [base-node quote-node base-node-qty]
   (let [finder (make-highest-weight-route-finder (make-one-way-graph base-node quote-node) (partial route-weight base-node-qty))
@@ -73,4 +64,4 @@
      :route route
      :rate (* 100 (/ (- return-qty base-node-qty) base-node-qty))}))
 
-(find-route (make-node :upbit "KRW") (make-node :binance "USDT") 100000000)
+(find-route (make-node :upbit "KRW") (make-node :binance "USDT") 500000)
