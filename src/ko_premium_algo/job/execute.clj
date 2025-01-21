11f7524be21597543557953ddf4d1e877afdb994
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2025-01-21 18:25:05 +0900

    etc: job trade 파일명 변경 및 불필요한 함수 내보내지 않음

--- src/ko_premium_algo/job/execute.clj
+++ src/ko_premium_algo/job/execute.clj
@@ -1,4 +1,4 @@
-(ns ko-premium-algo.job.trade
+(ns ko-premium-algo.job.execute
   (:require [ko-premium-algo.trade.intent :refer [make-intent market]]
             [ko-premium-algo.trade.market :refer [make-market]]
             [ko-premium-algo.route.edge :refer [start end weight]]
@@ -11,13 +11,13 @@
             [clojure.core.async :refer [chan go-loop >! <! timeout]]
             [ko-premium-algo.job.weight :refer [edge-weight]]))
 
-(defn trade-edge? [edge]
+(defn- trade-edge? [edge]
   (not= (:type (:meta edge)) :withdraw))
 
-(defn bid-edge? [edge]
+(defn- bid-edge? [edge]
   (= (:type (:meta edge)) :bid))
 
-(defn ask-edge->intent [edge qty]
+(defn- ask-edge->intent [edge qty]
   (make-intent (make-market (:asset (end edge))
                             (:asset (start edge))
                             (:symbol (:meta edge)))
@@ -25,7 +25,7 @@
                qty
                (/ 1 (weight edge))))
 
-(defn bid-edge->intent [edge qty]
+(defn- bid-edge->intent [edge qty]
   (make-intent (make-market (:asset (start edge))
                             (:asset (end edge))
                             (:symbol (:meta edge)))
@@ -33,24 +33,24 @@
                (/ qty (weight edge))
                (weight edge)))
 
-(defn edge->order-intent [edge qty]
+(defn- edge->order-intent [edge qty]
   ((if (bid-edge? edge)
      bid-edge->intent
      ask-edge->intent) edge qty))
 
-(defn edge->transfer-intent [edge qty]
+(defn- edge->transfer-intent [edge qty]
   (wallet-intent/make-intent "end exchange address"
                              (make-unit (:symbol (:meta edge))
                                         (:method (:meta edge)))
                              qty))
 
-(defn done-order? [exchange order-response]
+(defn- done-order? [exchange order-response]
   (= (state (order exchange (market (intent order-response)) (id order-response))) :done))
 
-(defn done-transfer? [exchange transfer-response]
+(defn- done-transfer? [exchange transfer-response]
   (= (transfer/state (transfer exchange (transfer/side transfer-response) (transfer/txid transfer-response))) "DONE"))
 
-(defn execute-edge [edge qty execute check]
+(defn- execute-edge [edge qty execute check]
   (let [response (execute edge qty)
         done-chan (chan 1)]
     (go-loop []
@@ -61,26 +61,26 @@
           (<! (timeout 1000))
           (recur))))))
 
-(defn execute-trade [edge qty]
+(defn- execute-trade [edge qty]
   (execute-order (:exchange (start edge)) (edge->order-intent edge qty)))
 
-(defn execute-transfer [edge qty]
+(defn- execute-transfer [edge qty]
   (execute-withdraw (:exchange (start edge)) (edge->transfer-intent edge qty)))
 
-(defn execute [edge]
+(defn- executer [edge]
   (if (trade-edge? edge)
     execute-trade
     execute-transfer))
 
-(defn check [edge]
+(defn- checker [edge]
   (let [exchange (:exchange (start edge))]
     (if (trade-edge? edge)
       (partial done-order? exchange)
       (partial done-transfer? exchange))))
 
-(defn trade [route qty]
+(defn execute [route qty]
   (go-loop [edges route
             qty qty]
     (when-let [edge (first edges)]
-      (<! (execute-edge edge qty (execute edge) (check edge)))
+      (<! (execute-edge edge qty (executer edge) (checker edge)))
       (recur (rest edges) ((edge-weight edge) qty)))))
