8d2c6a20dfed80058d28773831561f1277a2db4a
이진욱 <dlwlsdnr567@naver.com>
2025-01-27 19:41:22 +0900

    feat: signal
    이익을 위한 작업들의 순차열

---
+++ src/ko_premium_algo/job/signal.clj
@@ -0,0 +1,22 @@
+(ns ko-premium-algo.job.signal)
+
+(defn make-operation [op-type exchange intent]
+  {:op-type op-type :exchange exchange :intent intent})
+
+(defn op-type [operation]
+  (:op-type operation))
+
+(defn exchange [operation]
+  (:exchange operation))
+
+(defn intent [operation]
+  (:intent operation))
+
+(defn make-operation-seq [execute-type & operations]
+  {:execute-type execute-type :seq operations})
+
+(defn execute-type [operation-seq]
+  (:execute-type operation-seq))
+
+(defn operations [operation-seq]
+  (:seq operation-seq))
