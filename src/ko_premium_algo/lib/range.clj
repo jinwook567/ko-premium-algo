237a090c43023fa19e8c0a7608ab2cc6d918bccf
이진욱 <dlwlsdnr567@naver.com>
2025-02-04 22:00:47 +0900

    feat: coerce-step coerce-limit

--- src/ko_premium_algo/lib/range.clj
+++ src/ko_premium_algo/lib/range.clj
@@ -13,13 +13,26 @@
 (defn step [range]
   (:step range))
 
-(defn satisfy-step? [n step]
+(defn satisfy-step? [step n]
   (zero? (mod n step)))
 
-(defn satisfy-limit? [n min max]
+(defn satisfy-limit? [min max n]
   (and (>= n min)
        (<= n max)))
 
-(defn satisfy? [n range]
-  (and (satisfy-step? n (step range))
-       (satisfy-limit? n (min range) (max range))))
+(defn satisfy? [range n]
+  (and (satisfy-step? (step range) n)
+       (satisfy-limit? (min range) (max range) n)))
+
+(defn coerce-limit [min max n]
+  (cond
+    (< n min) min
+    (> n max) max
+    :else n))
+
+(defn coerce-step
+  ([step n] (coerce-step step n true))
+  ([step n floor?] (cond
+                     (satisfy-step? step n) n
+                     floor? (- n (mod n step))
+                     :else (+ (coerce-step step n) step))))
