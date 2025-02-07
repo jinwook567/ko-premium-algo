5c72e429cc5dd0d2eeaab9944027ce9a146ef386
이진욱 <dlwlsdnr567@naver.com>
2025-02-08 06:26:54 +0900

    refactor: binance ticker coll->query 함수 사용하도록 변경

--- src/ko_premium_algo/binance/ticker.clj
+++ src/ko_premium_algo/binance/ticker.clj
@@ -1,16 +1,13 @@
 (ns ko-premium-algo.binance.ticker
   (:require [ko-premium-algo.trade.ticker :refer [make-ticker]]
             [ko-premium-algo.trade.market :refer [symbol]]
+            [ko-premium-algo.binance.lib :refer [coll->query]]
             [cheshire.core :as json]
-            [clojure.string :as str]
             [clj-http.client :as client]))
-
-(defn- ticker-symbols [symbols]
-  (str "[" (str/join "," (map #(str "\"" % "\"") symbols)) "]"))
 
 (defn base-ticker [markets]
   (->> (map symbol markets)
-       (#(client/get "https://api.binance.com/api/v3/ticker/price" {:query-params {"symbols" (ticker-symbols %)}}))
+       (#(client/get "https://api.binance.com/api/v3/ticker/price" {:query-params {"symbols" (coll->query %)}}))
        (#(json/parse-string (:body %)))
        (map #(make-ticker %1 (Double/parseDouble (get %2 "price"))) markets)))
 
