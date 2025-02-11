38846228135d598a5c17b2eaf88025a363c399ae
이진욱 <dlwlsdnr567@naver.com>
2025-02-12 02:42:39 +0900

    feat: wallet make-address

---
+++ src/ko_premium_algo/wallet/address.clj
@@ -0,0 +1,10 @@
+(ns ko-premium-algo.wallet.address)
+
+(defn make-address [primary-address secondary-address]
+  {:primary-address primary-address :secondary-address secondary-address})
+
+(defn primary-address [address]
+  (:primary-address address))
+
+(defn secondary-address [address]
+  (:secondary-address address))
