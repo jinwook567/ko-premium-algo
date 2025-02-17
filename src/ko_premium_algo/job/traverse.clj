8a75973a71a6c4bddce3e0ea049f221b289ff7d9
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2025-02-17 17:12:34 +0900

    feat: 수수료 공제 관련 로직 적용

--- src/ko_premium_algo/job/traverse.clj
+++ src/ko_premium_algo/job/traverse.clj
@@ -1,6 +1,6 @@
 (ns ko-premium-algo.job.traverse
   (:require [ko-premium-algo.route.graph :refer [metadata]]
-            [ko-premium-algo.strategy.terms :refer [withdraw-qty order-qty coerce-range]]
+            [ko-premium-algo.strategy.terms :refer [withdraw-qty order-qty net-qty coerce-range]]
             [ko-premium-algo.trade.limits :refer [amount-range]]
             [ko-premium-algo.trade.terms :refer [limits]]
             [ko-premium-algo.lib.numeric :refer [precise]]))
@@ -9,11 +9,14 @@
   (let [meta (metadata edge)
         multiply (precise *)]
     (case (:type meta)
-      :withdraw (withdraw-qty (:base-terms meta) (:quote-terms meta) value)
-      :bid (order-qty (:terms meta) (:price meta) value)
+      :withdraw (->> (withdraw-qty (:base-terms meta) (:quote-terms meta) value)
+                     (net-qty (:base-terms meta)))
+      :bid (->> (order-qty (:terms meta) (:price meta) value)
+                (net-qty (:terms meta)))
       :ask (->> (order-qty (:terms meta) (:price meta) (multiply (:price meta) value))
                 (multiply (:price meta))
-                (coerce-range (amount-range (limits (:terms meta))))))))
+                (coerce-range (amount-range (limits (:terms meta))))
+                (net-qty (:terms meta))))))
 
 (defn traverse-route [route value]
   (reduce #(traverse-edge %2 %1) value route))
--- src/ko_premium_algo/strategy/terms.clj
+++ src/ko_premium_algo/strategy/terms.clj
@@ -1,7 +1,7 @@
 (ns ko-premium-algo.strategy.terms
   (:refer-clojure :exclude [min max])
   (:require
-   [ko-premium-algo.trade.fee :refer [coerce-fee]]
+   [ko-premium-algo.trade.fee :refer [deduct gross]]
    [ko-premium-algo.trade.terms :refer [fee limits]]
    [ko-premium-algo.trade.limits :refer [qty-range amount-range]]
    [ko-premium-algo.wallet.limits :as wallet-limit :refer [can-transfer? actions]]
@@ -16,12 +16,15 @@
 
 (defn order-qty [terms price amount]
   (->> amount
-       (coerce-fee (fee terms))
+       (gross (fee terms))
        (coerce-range (amount-range (limits terms)))
        (#(/ % price))
        (coerce-range (qty-range (limits terms)))))
 
 (defn withdraw-qty [base-terms quote-terms qty]
   (->> (if (can-transfer? (actions (wallet-terms/limits base-terms)) (actions (wallet-terms/limits quote-terms))) qty 0)
-       (coerce-fee (fee base-terms))
+       (gross (fee base-terms))
        (coerce-range (wallet-limit/qty-range (wallet-terms/limits base-terms)))))
+
+(defn net-qty [terms qty]
+  (deduct (fee terms) qty))
