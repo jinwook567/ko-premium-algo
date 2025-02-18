c37dc187e4d8c76d176e3cf8ac980b4675c58044
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2025-02-18 22:51:11 +0900

    feat: 거래대금 하위 30%인 edge 제외

--- src/ko_premium_algo/job/search.clj
+++ src/ko_premium_algo/job/search.clj
@@ -1,13 +1,15 @@
 (ns ko-premium-algo.job.search
   (:require [ko-premium-algo.trade.market :refer [base-asset]]
+            [ko-premium-algo.chart.candle :refer [trading-value]]
             [ko-premium-algo.job.edge :refer [make-ask-edge make-bid-edge make-withdraw-edge]]
             [ko-premium-algo.job.traverse :refer [traverse-route]]
             [ko-premium-algo.route.search :refer [higher-choice optimal-route]]
             [ko-premium-algo.route.graph :refer [edges->graph metadata]]
             [ko-premium-algo.gateway.markets :refer [markets]]
-            [ko-premium-algo.gateway.ticker :refer [ticker]]
+            [ko-premium-algo.gateway.ticker :refer [ticker candle-ticker]]
             [ko-premium-algo.gateway.terms :refer [terms]]
             [ko-premium-algo.gateway.transfer :as transfer]
+            [ko-premium-algo.lib.seq :refer [percent-idx]]
             [clojure.math.combinatorics :as combo]))
 
 (defn make-exchange-edges [exchange markets]
@@ -27,13 +29,24 @@
        (mapcat #(make-bridge-edges (first %) (second %) (transfer/units (first %))))
        (filter #(some? (:base-terms (metadata %))))))
 
-(defn base-asset-edges [node]
+(defn base-asset-markets [node]
   (->> (markets (:exchange node))
-       (filter #(= (base-asset %) (:asset node)))
+       (filter #(= (base-asset %) (:asset node)))))
+
+(defn liquid-markets [exchange markets]
+  (->> (candle-ticker exchange markets)
+       (map vector markets)
+       (sort-by #(trading-value (second %)))
+       (map first)
+       (#(drop (inc (percent-idx 30 %)) %))))
+
+(defn linked-liquid-edges [node]
+  (->> (base-asset-markets node)
+       (liquid-markets (:exchange node))
        (make-exchange-edges (:exchange node))))
 
 (defn base-asset-graph [& nodes]
-  (edges->graph (concat (mapcat base-asset-edges nodes)
+  (edges->graph (concat (mapcat linked-liquid-edges nodes)
                         (apply link-exchanges (set (map :exchange nodes))))))
 
 (defn make-route-finder [graph]
--- src/ko_premium_algo/lib/seq.clj
+++ src/ko_premium_algo/lib/seq.clj
@@ -9,3 +9,10 @@
   ([list] list)
   ([list1 list2] (for [x list1 y list2] (list x y)))
   ([list1 list2 & more] (reduce #(map flatten (cartesian-product %1 %2)) list1 (conj more list2))))
+
+(defn percent-idx [percent seq]
+  (->> (count seq)
+       (* (/ percent 100))
+       int
+       dec
+       (max 0)))
